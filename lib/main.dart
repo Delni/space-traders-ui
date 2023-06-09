44b5c11cdd910dd3a7385b767ef0b2be19401832
Nicolas DELAUNEY <niko-d@live.fr>
2023-06-09 10:10:45 +0200

    🏗️ Use routes if some day we want to go web

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:space_traders/infra-ui/pages/home/home.dart';
 import 'package:space_traders/infra-ui/pages/login/login.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
+Map<String, WidgetBuilder> routes = {
+  '/home': (context) => const HomePage(),
+};
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginPage()
+    return MaterialApp(
+      home: const LoginPage(),
+      routes: routes,
     );
   }
 }
